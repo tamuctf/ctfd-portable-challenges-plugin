8e7894904194588c5322b80a45017661d6153a1e
Victor Nate Graf <vnategraf@gmail.com>
2017-03-11 10:59:49 -0600

    move code to function

--- importer.py
+++ importer.py
@@ -48,6 +48,127 @@ class MissingFieldError(Exception):
     def __str__(self):
         return "Error: Missing field '{}'".format(name)
 
+def import_challenges():
+    chals = []
+    with open(args.in_file, 'r') as in_stream:
+        chals = yaml.safe_load_all(in_stream)
+
+        for chal in chals:
+            skip = False
+            for req_field in req_fields:
+                if req_field not in chal:
+                    if args.exit_on_error:
+                        raise MissingFieldError(req_field)
+                    else:
+                        print "Skipping challenge: Missing field '{}'".format(req_field)
+                        skip = True
+                        break
+            if skip:
+                continue
+
+            for flag in chal['flags']:
+                if 'flag' not in flag:
+                    if args.exit_on_error:
+                        raise MissingFieldError('flag')
+                    else:
+                        print "Skipping flag: Missing field 'flag'"
+                        continue
+                flag['flag'] = flag['flag'].strip()
+                if 'type' in flag == "REGEX":
+                    flag['type'] = 1
+                else:
+                    flag['type'] = 0
+
+            # We ignore traling and leading whitespace when importing challenges
+            chal_dbobj = Challenges(
+                chal['name'].strip(),
+                chal['description'].strip(),
+                chal['value'],
+                chal['category'].strip(),
+                chal['flags']
+            )
+
+            if 'hidden' in chal and chal['hidden']:
+                chal_dbobj.hidden = True
+
+            matching_chals = Challenges.query.filter_by(
+                name=chal_dbobj.name,
+                description=chal_dbobj.description,
+                value=chal_dbobj.value,
+                category=chal_dbobj.category,
+                hidden=chal_dbobj.hidden
+            ).all()
+
+            for match in matching_chals:
+                if 'tags' in chal:
+                    tags_db = [tag.tag for tag in Tags.query.add_columns('tag').filter_by(chal=match.id).all()]
+                    if all([tag not in tags_db for tag in chal['tags']]):
+                        continue
+                if 'files' in chal:
+                    files_db = [f.location for f in Files.query.add_columns('location').filter_by(chal=match.id).all()]
+                    if len(files_db) != len(chal['files']):
+                        continue
+
+                    hashes = []
+                    for file_db in files_db:
+                        with open(os.path.join(args.out_file_dir, file_db), 'r') as f:
+                            hash = hashlib.md5(f.read()).digest()
+                            hashes.append(hash)
+
+                    mismatch = False
+                    for file in chal['files']:
+                        filepath = os.path.join(os.path.dirname(args.in_file), file)
+                        with open(filepath, 'r') as f:
+                            hash = hashlib.md5(f.read()).digest()
+                            if hash in hashes:
+                                hashes.remove(hash)
+                            else:
+                                mismatch = True
+                                break
+                    if mismatch:
+                        continue
+
+                if json.loads(match.flags) == chal['flags']:
+                    skip = True
+                    break
+            if skip:
+                print "Skipping {}: Duplicate challenge found in DB".format(chal['name'].encode('utf8'))
+                continue
+
+            print "Adding {}".format(chal['name'].encode('utf8'))
+            db.session.add(chal_dbobj)
+            db.session.commit()
+
+            if 'tags' in chal:
+                for tag in chal['tags']:
+                    tag_dbobj = Tags(chal_dbobj.id, tag)
+                    db.session.add(tag_dbobj)
+
+            if 'files' in chal:
+                for file in chal['files']:
+                    filename = os.path.basename(file)
+                    dstfilename = secure_filename(filename)
+
+                    md5hash = hashlib.md5(os.urandom(64)).hexdigest()
+                    dstdir = os.path.join(args.out_file_dir, md5hash)
+
+                    while os.path.exists(dstdir):
+                        md5hash = hashlib.md5(os.urandom(64)).hexdigest()
+                        dstdir = os.path.join(file_dir, md5hash)
+
+                    os.makedirs(dstdir)
+                    dstpath = os.path.join(dstdir, dstfilename)
+                    srcpath = os.path.join(os.path.dirname(args.in_file), file)
+
+                    shutil.copy(srcpath, dstpath)
+                    file_dbobj = Files(chal_dbobj.id, os.path.relpath(dstpath, start=args.out_file_dir))
+
+                    db.session.add(file_dbobj)
+
+    db.session.commit()
+    db.session.close()
+        
+
 if __name__ == "__main__":
     args = parse_args()
     
@@ -76,122 +197,4 @@ if __name__ == "__main__":
             db.create_all()
 
         app.db = db
-
-        chals = []
-        with open(args.in_file, 'r') as in_stream:
-            chals = yaml.safe_load_all(in_stream)
-
-            for chal in chals:
-                skip = False
-                for req_field in req_fields:
-                    if req_field not in chal:
-                        if args.exit_on_error:
-                            raise MissingFieldError(req_field)
-                        else:
-                            print "Skipping challenge: Missing field '{}'".format(req_field)
-                            skip = True
-                            break
-                if skip:
-                    continue
-
-                for flag in chal['flags']:
-                    if 'flag' not in flag:
-                        if args.exit_on_error:
-                            raise MissingFieldError('flag')
-                        else:
-                            print "Skipping flag: Missing field 'flag'"
-                            continue
-                    flag['flag'] = flag['flag'].strip()
-                    if 'type' in flag == "REGEX":
-                        flag['type'] = 1
-                    else:
-                        flag['type'] = 0
-
-                # We ignore traling and leading whitespace when importing challenges
-                chal_dbobj = Challenges(
-                    chal['name'].strip(),
-                    chal['description'].strip(),
-                    chal['value'],
-                    chal['category'].strip(),
-                    chal['flags']
-                )
-
-                if 'hidden' in chal and chal['hidden']:
-                    chal_dbobj.hidden = True
-
-                matching_chals = Challenges.query.filter_by(
-                    name=chal_dbobj.name,
-                    description=chal_dbobj.description,
-                    value=chal_dbobj.value,
-                    category=chal_dbobj.category,
-                    hidden=chal_dbobj.hidden
-                ).all()
-
-                for match in matching_chals:
-                    if 'tags' in chal:
-                        tags_db = [tag.tag for tag in Tags.query.add_columns('tag').filter_by(chal=match.id).all()]
-                        if all([tag not in tags_db for tag in chal['tags']]):
-                            continue
-                    if 'files' in chal:
-                        files_db = [f.location for f in Files.query.add_columns('location').filter_by(chal=match.id).all()]
-                        if len(files_db) != len(chal['files']):
-                            continue
-
-                        hashes = []
-                        for file_db in files_db:
-                            with open(os.path.join(args.out_file_dir, file_db), 'r') as f:
-                                hash = hashlib.md5(f.read()).digest()
-                                hashes.append(hash)
-
-                        mismatch = False
-                        for file in chal['files']:
-                            filepath = os.path.join(os.path.dirname(args.in_file), file)
-                            with open(filepath, 'r') as f:
-                                hash = hashlib.md5(f.read()).digest()
-                                if hash in hashes:
-                                    hashes.remove(hash)
-                                else:
-                                    mismatch = True
-                                    break
-                        if mismatch:
-                            continue
-
-                    if json.loads(match.flags) == chal['flags']:
-                        skip = True
-                        break
-                if skip:
-                    print "Skipping {}: Duplicate challenge found in DB".format(chal['name'].encode('utf8'))
-                    continue
-
-                print "Adding {}".format(chal['name'].encode('utf8'))
-                db.session.add(chal_dbobj)
-                db.session.commit()
-
-                if 'tags' in chal:
-                    for tag in chal['tags']:
-                        tag_dbobj = Tags(chal_dbobj.id, tag)
-                        db.session.add(tag_dbobj)
-
-                if 'files' in chal:
-                    for file in chal['files']:
-                        filename = os.path.basename(file)
-                        dstfilename = secure_filename(filename)
-
-                        md5hash = hashlib.md5(os.urandom(64)).hexdigest()
-                        dstdir = os.path.join(args.out_file_dir, md5hash)
-
-                        while os.path.exists(dstdir):
-                            md5hash = hashlib.md5(os.urandom(64)).hexdigest()
-                            dstdir = os.path.join(file_dir, md5hash)
-
-                        os.makedirs(dstdir)
-                        dstpath = os.path.join(dstdir, dstfilename)
-                        srcpath = os.path.join(os.path.dirname(args.in_file), file)
-
-                        shutil.copy(srcpath, dstpath)
-                        file_dbobj = Files(chal_dbobj.id, os.path.relpath(dstpath, start=args.out_file_dir))
-
-                        db.session.add(file_dbobj)
-
-        db.session.commit()
-        db.session.close()
+        import_challenges()
